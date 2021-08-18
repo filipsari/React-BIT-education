ab5141a45afb743c819b87123d3f75d6dcf78fad
FilipSari <filipsari@gmail.com>
2021-08-18 21:51:19 +0200

    bit react practise- work in progress

---
+++ react-exerices-BIT/src/App.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import {Header} from './components/Header/Header'
+import {Footer} from './components/Footer/Footer'
+
+
+function App() {
+  return (
+    <Fragment> 
+    <Header/> 
+    <Footer/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ react-exerices-BIT/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+export const Header = () => {
+  return (
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a class="waves-effect waves-light btn">Button</a></li>
+        <li><a class="waves-effect waves-light btn">Button <i class="material-icons right">cloud</i></a></li>
+        <li><a class="waves-effect waves-light btn-large">Large Button</a></li>
+      </ul>
+    </div>
+  </nav>
+   );
+
+}
+
